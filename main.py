fe65725948990d94e3333c32bef8a707378b21b7
Matthew <96619454+LichasX@users.noreply.github.com>
2024-03-01 01:46:10 +0000

    final submission

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 num = input('Enter a number (decimal only): ')
-# type your code here
-
-
-
+for x, y in enumerate(num):
+  if y == ".":
+    dp = len(num[x+1::])
 # do not change any code beyond this point
 print('The number', num, 'has', dp, 'decimal places.')
